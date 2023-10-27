08849166cee358e28ef2619c0d053d238c532bc9
Peyman <p.zonouz@gmail.com>
2023-10-27 23:58:58 +0330

    products classification changed

--- app/product/[name]/page.jsx
+++ app/product/[name]/page.jsx
@@ -35,14 +35,14 @@ const page = ({ params }) => {
       <div className=" flex w-full flex-col items-center justify-center bg-red-600 h-28 text-xl text-white text-center align-middle ">
         {name}
       </div>
-      <img src={product.image} />
+      <img src={product.image} alt={product.name} />
       <div className=" px-4 text-center  ">{product.description}</div>
       <div className=" mt-20 p-2 bg-gray-200 w-full flex flex-col gap-3">
         <i className=" mt-6 fa fa-phone fa-5x text-center"></i>
         <div className=" mt-6"> نیاز به مشاوره دارید؟</div>
         <div className="mt-6 text-gray-600 ">
           جهت استعلام قیمت محصولات، مشاوره خرید و هر سوال دیگر میتوانید با ما
-          بطور مستقیم در ارتباط باشید.
+          بطور مستقیم در ارتباط دارید.
         </div>
         <div className=" text-center text-gray-600 text-2xl font-bold tracking-wide">
           09146160216
