51434d2e77ecc45d450a2a555b92d2ca33b839ec
Peyman <p.zonouz@gmail.com>
2023-11-25 21:01:37 +0330

    visit card reverse

--- app/components/VisitCards.jsx
+++ app/components/VisitCards.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const VisitCards = () => {
   return (
-    <div className=" flex flex-col md:flex-row bg-gray-300 md:justify-around  mt-10 mx-[1rem] p-[1rem] gap-[1rem] md:col-span-full">
+    <div className=" flex flex-col-reverse md:flex-row-reverse bg-gray-300 md:justify-around  mt-10 mx-[1rem] p-[1rem] gap-[1rem] md:col-span-full">
       <Image width={600} height={300} src="/1-2-600x360.jpg" alt="VisitCard" />
       <Image width={600} height={300} src="/2-2-600x360.jpg" alt="VisitCard" />
     </div>
